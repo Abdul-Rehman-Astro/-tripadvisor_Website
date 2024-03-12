3ba33ffaf6ad2f06e9ad3b7bbb551164cefeb513
Abdul-Rehman-Astro <ab.rehman01280@gmail.com>
2024-03-12 11:46:05 +0530

    First Commit to Website

---
+++ script.js
@@ -0,0 +1,6 @@
+function upload() {
+    var lat = document.getElementById('latitude').value;
+    var long = document.getElementById('longitude').value;
+    // Add functionality to use these coordinates to display location on map or other purposes
+    alert('Latitude: ' + lat + ', Longitude: ' + long);
+ }
